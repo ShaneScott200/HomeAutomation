c60ac119a79bec023958076ad2ca29c8fdf7d1a4
Shane Scott <33729291+ShaneScott200@users.noreply.github.com>
2019-01-08 12:50:15 -0500

    Update timeServer.cpp

--- timeServer.cpp
+++ timeServer.cpp
@@ -12,7 +12,7 @@ int main()
     std::cout << std:: asctime(std::localtime(&result)) << result << " seconds since the Epoch\n";  // replace this with the mqtt.publish function
    
     mosqpp::mosquittopp* m = new mosqpp::mosquittopp("Test", false);
-    m->connect("192.168.0.101", 1883, 60);
+    m->connect("192.168.0.200", 1883, 60);
     char resultstring[32];
     sprintf(resultstring, "%ld", result);
     m->publish(NULL, "time", 32, resultstring, 0, false);
